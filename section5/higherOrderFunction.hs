d9d67a7aaebb7115c4208f511bd8e709cc1dacba
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2012-06-04 17:01:25 +0900

    add section 5

---
+++ section5/higherOrderFunction.hs
@@ -0,0 +1,18 @@
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ _ [] = []
+zipWith' _ [] _ = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x
+
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f x y = f y x
+
