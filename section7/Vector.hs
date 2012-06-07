0132671c536e14457f2fba3a92f4ad4298a1a3be
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2012-06-07 11:11:40 +0900

    add Vector car car' to section7

---
+++ section7/Vector.hs
@@ -0,0 +1,16 @@
+module Vector 
+    ( Vector
+    , vplus
+    , dotProd
+    , vmult
+    ) where
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i * l + j * m + k * n
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i * m) (j * m) (k * m) 
