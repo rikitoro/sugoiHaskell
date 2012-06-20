2289bc35c87450841cd3b0e927b479e2d8c1c80c
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2012-06-20 17:49:13 +0900

    add section 15 and add Tree on section15

---
+++ section15/Tree.hs
@@ -0,0 +1,27 @@
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
+
+freeTree :: Tree Char
+freeTree =
+    Node 'P'
+             (Node 'O'
+                       (Node 'L' 
+                                 (Node 'N' Empty Empty)
+                                 (Node 'T' Empty Empty)
+                       )
+                       (Node 'T' 
+                                 (Node 'S' Empty Empty)
+                                 (Node 'A' Empty Empty)
+                       )
+
+             )
+             (Node 'L'
+                       (Node 'W' 
+                                 (Node 'C' Empty Empty)
+                                 (Node 'R' Empty Empty)
+                       )
+                       (Node 'A' 
+                                 (Node 'A' Empty Empty)
+                                 (Node 'C' Empty Empty)
+                       )
+
+             )
