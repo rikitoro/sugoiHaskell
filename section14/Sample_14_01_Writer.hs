7fad7df2e1a171308cd28aacaf0b0e247f505f6f
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2014-09-13 20:19:23 +0900

    add some specs to Sample_14_01_DiffList

--- section14/Sample_14_01_Writer.hs
+++ section14/Sample_14_01_Writer.hs
@@ -17,3 +17,28 @@ multWithLog' = do
   b <- logNumber 5
   tell ["Gonna multiply these two"]
   return (a*b)
+
+gcd' :: Int -> Int -> Int
+gcd' a b
+  | b == 0    = a
+  | otherwise = gcd' b (a `mod` b)
+
+gcd'' :: Int -> Int -> Writer [String] Int
+gcd'' a b
+  | b == 0    = do
+    tell ["Finished with " ++ show a]
+    return a
+  | otherwise = do
+    tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    gcd'' b (a `mod` b)
+
+gcdReverse :: Int -> Int -> Writer [String] Int
+gcdReverse a b
+  | b == 0    = do
+    tell ["Finished with " ++ show a]
+    return a
+  | otherwise = do
+    result <- gcdReverse b (a `mod` b)
+    tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    return result
+
