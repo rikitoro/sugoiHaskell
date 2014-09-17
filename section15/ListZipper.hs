4028958a62afffd1cb3a9e0411c3a8919ab5a6fa
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2014-09-17 23:04:20 +0900

    add specs to ListZipper

--- section15/ListZipper.hs
+++ section15/ListZipper.hs
@@ -1,7 +1,12 @@
+module ListZipper where
+
 type ListZipper a = ([a], [a])
 
 goForward :: ListZipper a -> ListZipper a
 goForward (x:xs, bs) = (xs, x:bs)
 
-goBackward :: ListZipper a -> ListZipper a
-goBackward (xs, b:bs) = (b:xs, bs)
+goBack :: ListZipper a -> ListZipper a
+goBack (xs, b:bs) = (b:xs, bs)
+
+(-:) :: a -> (a -> b) -> b
+x -: f = f x
